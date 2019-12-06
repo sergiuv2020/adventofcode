2ace2f1cb34612fb947c439a0301a272f51f5ab5
svidrascu <sergiu.vidrascu@ness.com>
2019-12-06 11:26:29 +0200

    day1 expanded

--- day1-gif-me-my-fuel.py
+++ day1-gif-me-my-fuel.py
@@ -1,5 +1,9 @@
 with open('day1inputs.txt', 'r') as file:
     masses = file.read().splitlines()
 totalMass=0
-for mass in masses: totalMass += int(mass) // 3 - 2
+# for mass in masses: totalMass += int(mass) // 3 - 2 #first one
+for mass in masses:
+    while int(mass) > 0:
+        mass = int(mass) // 3 - 2
+        if mass > 0: totalMass += mass
 print(totalMass)
